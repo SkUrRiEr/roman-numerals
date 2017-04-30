4e67b99914fab8805f0bf500db6abdd4d5a7d928
Julian Calaby <julian.calaby@gmail.com>
2017-04-30 15:58:30 +1000

    Add a map of roman numerals and the values they represent

--- src/RomanNumeralGenerator.php
+++ src/RomanNumeralGenerator.php
@@ -8,6 +8,25 @@ namespace Larowlan\RomanNumeral;
 class RomanNumeralGenerator {
 
   /**
+   * @var array Map of values and the roman numerals that represent them
+   */
+  private static $values = array(
+    1000 => "M",
+    900 => "CM",
+    500 => "D",
+    400 => "CD",
+    100 => "C",
+    90 => "XC",
+    50 => "L",
+    40 => "XL",
+    10 => "X",
+    9 => "IX",
+    5 => "V",
+    4 => "IV",
+    1 => "I",
+  );
+
+  /**
    * Generates a roman numeral from an integer.
    *
    * @param int $number
